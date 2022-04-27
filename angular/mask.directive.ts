e3464cffe048742afeae14b8880b9de78b002058
Gorky Rojas <gorkyrojas@gmail.com>
2022-04-26 23:36:56 -0400

    angular: format -> mask directive

--- angular/mask.directive.ts
+++ angular/mask.directive.ts
@@ -2,23 +2,22 @@ import { Directive, ElementRef, HostListener, Input } from "@angular/core";
 import { NgModel } from "@angular/forms";
 
 @Directive({
-	selector: '[format]'
+	selector: '[mask]'
 })
-export class InputFormatterDirective {
+export class InputMaskerDirective {
 	private element: HTMLInputElement;
 	constructor(private eref: ElementRef, private model: NgModel) {
 		this.element = eref.nativeElement;
 	}
 
-	@Input() format: string;
+	@Input() mask: string;
 	get validators(): ((_: string) => boolean)[] {
-		const validator_chars = Object.keys(validators);
-		return [...this.format]
-			.filter(f => validator_chars.includes(f))
-			.map(f => validators[f]);
+		return [...this.mask]
+			.filter(m => m in masks)
+			.map(f => masks[f]);
 	}
 
-	get constrained_value(): string {
+	get unmasked_value(): string {
 		const value = this.element.value;
 		const { value: constrained_value } = [...value].reduce(({ value, validators }, char) => {
 			if (validators.length && validators[0](char))
@@ -29,30 +28,29 @@ export class InputFormatterDirective {
 		return constrained_value;
 	}
 
-	@HostListener('input') handle_input() {
-		let constrained_value = this.constrained_value;
-		if (!constrained_value) {
+	@HostListener('input') onInput() {
+		let unmasked = this.unmasked_value;
+		if (!unmasked) {
 			this.element.value = '';
 			return;
 		}
-		const constrains = Object.keys(validators);
 		let formatted_value = '';
-		for (let f of this.format) {
-			if (!constrained_value) break;
+		for (let f of this.mask) {
+			if (!unmasked) break;
 
-			if (!constrains.includes(f))
+			if (f in masks) {
+				formatted_value += unmasked[0];
+				unmasked = unmasked.substring(1);
+
+			} else 
 				formatted_value += f;
-			else {
-				formatted_value += constrained_value[0];
-				constrained_value = constrained_value.substring(1);
-			}
 		}
 		this.element.value = formatted_value;
 		this.model?.viewToModelUpdate(formatted_value);
 	}
 }
 
-const validators: { [char: string]: (char: string) => boolean } = {
+const masks: { [char: string]: (char: string) => boolean } = {
 	'#': _ => /[0-9]/.test(_),
 	'a': _ => /[A-z]/.test(_),
 	'.': _ => /[A-z0-9]/.test(_)
